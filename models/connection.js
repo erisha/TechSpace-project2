a01a04c7c5d9a8e581d3efe41195e5a7eca55886
erisha <forever.erisha@gmail.com>
2022-07-20 14:51:32 -0400

    fixed heroku

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Fire off the connection to Mongo DB
-mongoose.connect(process.env.DATABASE_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
